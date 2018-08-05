89b5a8b7ca20ce71247a03525d4f7c034ab2ee40
Wade Wegner <wade.wegner@gmail.com>
2018-08-05 09:09:56 -0500

    Fixed command execution

--- src/commands/tags.ts
+++ src/commands/tags.ts
@@ -9,34 +9,27 @@ export default class Tags extends Command {
     year: flags.string({char: 'y', description: 'year', default: '2018'})
   }
 
-  static args = [{name: 'file'}]
   async run() {
+   
     const {args, flags} = this.parse(Tags)
     const url = `https://www.thatconference.com/api3/Session/GetAcceptedSessions?year=${flags.year}`
+    
     let {data} = await axios.get(url)
-
-
-    var allTags:string[] = new Array() 
+    let allTags:string[] = new Array() 
 
     for (let user of data) {
-
       let tags = user.Tags
-
 
       for (let tag of tags.keys()) {
         let name = tags[tag];
         allTags.push(name)
       }
-
     }
 
-    // this.log(allTags.count)
-
     var uniqueTags = Array.from(new Set(allTags))
-    // this.log(uniqueTags.count)
 
     for (let tag of uniqueTags) {
-      // this.log(tag.Name)
+      this.log(tag.Name)
     }
   }
 }
